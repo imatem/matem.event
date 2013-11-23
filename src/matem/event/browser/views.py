61dfa031ff376564e1487bce08b8f4479e74af88
Adriana Ramirez V <adriana.rv@gmail.com>
2013-11-23 14:21:02 -0600

    Change views for news and events

--- src/matem/event/browser/views.py
+++ src/matem/event/browser/views.py
@@ -4,4 +4,5 @@ from Products.Collage.browser.views import BaseTopicView
 from Products.Collage.utilities import CollageMessageFactory as _
 
 class IMStandardTopicView(BaseTopicView):
-    title = _(u'IM Standard')
+    pass
+    #title = _(u'IM Standard')
