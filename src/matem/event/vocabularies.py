5a5a833d67e7d81715499fee0572e7ba2082fcf3
adriana-rv <adriana.rv@gmail.com>
2014-03-03 16:18:55 -0600

    Add folder view calendar

--- src/matem/event/vocabularies.py
+++ src/matem/event/vocabularies.py
@@ -92,6 +92,27 @@ def PeriodicitySeminar(context):
 directlyProvides(PeriodicitySeminar, IVocabularyFactory)
 
 
+def Months(context):
+
+    items = [
+        (_(u'January'), 1),
+        (_(u'February'), 2),
+        (_(u'March'), 3),
+        (_(u'April'), 4),
+        (_(u'May'), 5),
+        (_(u'June'), 6),
+        (_(u'July'), 7),
+        (_(u'August'), 8),
+        (_(u'September'), 9),
+        (_(u'October'), 10),
+        (_(u'November'), 11),
+        (_(u'December'), 12),
+    ]
+
+    #value, token, título
+    items = [SimpleTerm(i[1], i[1], i[0]) for i in items]
+    return SimpleVocabulary(items)
+directlyProvides(Months, IVocabularyFactory)
 
 
 
