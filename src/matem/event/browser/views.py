ca5343f5a94b571cfc173abc5ff2828f1b90f8f1
adriana-rv <adriana.rv@gmail.com>
2016-08-18 17:52:36 -0500

    Add page with comments view in collage

--- src/matem/event/browser/views.py
+++ src/matem/event/browser/views.py
@@ -91,3 +91,86 @@ class IMEventView(BaseTopicView):
 
         return nationality
         # return getattr(self.context, 'speaker_nationality', None)
+
+
+from Acquisition import aq_inner, aq_base
+from AccessControl import getSecurityManager
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from Products.CMFDefault.DiscussionTool import DiscussionNotAllowed
+
+from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getMultiAdapter
+
+from plone.app.discussion.interfaces import IConversation
+from DateTime import DateTime
+
+
+
+
+class IMPageCommentView(BaseTopicView):
+
+    # Methods of
+    def can_reply(self):
+        return getSecurityManager().checkPermission('Reply to item', aq_inner(self.context))
+
+
+    def getComments(self, workflow_actions=True):
+        """Returns all replies to a content object.
+
+        If workflow_actions is false, only published
+        comments are returned.
+
+        If workflow actions is true, comments are
+        returned with workflow actions.
+        """
+        context = aq_inner(self.context)
+        conversation = IConversation(context, None)
+
+        if conversation is None:
+            return iter([])
+
+        wf = getToolByName(context, 'portal_workflow')
+
+        # workflow_actions is only true when user
+        # has 'Manage portal' permission
+
+        def replies_with_workflow_actions():
+            # Generator that returns replies dict with workflow actions
+            for r in conversation.getThreads():
+                comment_obj = r['comment']
+                # list all possible workflow actions
+                actions = [
+                    a for a in wf.listActionInfos(object=comment_obj)
+                    if a['category'] == 'workflow' and a['allowed']
+                ]
+                r = r.copy()
+                r['actions'] = actions
+                yield r
+
+        def published_replies():
+            # Generator that returns replies dict with workflow status.
+            for r in conversation.getThreads():
+                comment_obj = r['comment']
+                workflow_status = wf.getInfoFor(comment_obj, 'review_state')
+                if workflow_status == 'published':
+                    r = r.copy()
+                    r['workflow_status'] = workflow_status
+                    yield r
+
+        # Return all direct replies
+        lenvalue = len(conversation.objectIds())
+
+
+        if len(conversation.objectIds()):
+            if workflow_actions:
+                return replies_with_workflow_actions()
+            else:
+                return published_replies()
+
+    def format_time(self, time):
+        # We have to transform Python datetime into Zope DateTime
+        # before we can call toLocalizedTime.
+        util = getToolByName(self.context, 'translation_service')
+        zope_time = DateTime(time.isoformat())
+        return util.toLocalizedTime(zope_time, long_format=True)
